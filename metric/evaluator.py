48dadb102298a8b92c13568c9d1924a047b90127
nils-hde <nils.hilgers@rwth-aachen.de>
2023-09-22 10:40:57 +0000

    Functional restructuring, prompt building implemented and test added

--- metric/evaluator.py
+++ metric/evaluator.py
@@ -1,26 +1,36 @@
-from llama2_local import Llama2Local
-from metric.scorer import LLEvaluator
 import numpy as np
 from utils import add_prompt_instructions, print_scores
-
-# Define abstract class prompt evaluator which should define methods to build and pass a prompt for evaluation to the local model,
-# outputs a score and an explanation for a given input dialog history, knowledge string, and two candidate responses.
-class PromptEvaluator:
-    def build_prompt(self, metric, context, response_1, response_2=None, method="winrate"):
-        raise NotImplementedError()
-    
-    def submit_prompt(self, prompt, method, metric):
-        raise NotImplementedError()
+import random
 
 class DialogEvaluator:
-    def __init__(self, max_length=1024, device='cuda:0', cache_dir=None):
+    def __init__(self, prompt_scorer, max_length=1024, device='cuda:0', cache_dir=None):
         """ Set up evaluator for dialogues """
-        llama2_local = Llama2Local(api_url='http://gpu-19.apptek.local:8080/generate')
-        self.scorer = LLEvaluator(model_prompter=llama2_local, 
-                                   max_length=max_length, 
-                                   device=device, cache_dir=cache_dir)
+        self.scorer = prompt_scorer
         self.task = 'dialogue'
-        self.dimensions = ['accuracy', 'appropriateness']
+        self.dimension_map = {
+            'appropriate': {
+                'description': 'Rate how appropriate this response is. An appropriate response is naturally connected to the previous turns and helpful for the user\'s concern. Do not consider factual correctness.',
+            },
+            'accurate': {
+                'description': 'Rate how accurate this response is. An accurate response is factually correct and consistent with the knowledge in the context.',
+                'data_specific_task_description': 'Context can be reviews from customers or FAQs. FAQs start after token :F: and each new review starts after token :R:.'
+            },
+        }
+        self.dimension = ["appropriate", "accurate"]
+        self.init_prompt = "[INST] <<SYS>>"
+        self.task_prompt = "For the following multi-turn conversation between User and Assistant, you will be given a potential response for the next turn."
+        self.context_prompt = "<</SYS>> {}"
+        self.candidate_prompt = "\n\n## Response\n{}\n\n## "
+        self.eval_prompt = " Task\nFIRST provide a one-sentence explanation of your rating. SECOND, state only state only the score on a scale of 1 to 5. Follow the template.\n\n## Template\nExplanation: <one-sentence explanation>\n{} Score: <1-5>"
+        self.post_prompt = "[/INST]Explanation:"
+
+    def build_dim_prompt(self, dimension, context, response_1, response_2):
+        # Concatenate init, task, selected dimensions and post prompts
+        prompt = self.init_prompt + self.task_prompt + self.selected_dimensions[dimension]['description'] + \
+                    self.selected_dimensions[dimension]['data_specific_task_description'] + self.post_prompt
+        # Now format the prompt with the context, response_1 and response_2
+        prompt = prompt.format(context, response_1, response_2, dimension.capitalize())
+        return prompt
 
     def evaluate(self, data, dims=None, overall=True, print_result=False):
         """
@@ -47,21 +57,13 @@ class DialogEvaluator:
         for dim in eval_dims:
             print('Evaluating {} of {} samples !!!'.format(dim, n_data))
             
-            # Calculate turn-level score for other dimensions
-            if dim in ['accuracy', 'appropriateness']:
-                src_list, output_list, context_list = [], [], []
-                for i in range(n_data):
-                    src_list.append(data[i]['source'])
-                    output_list.append(data[i]['system_output'])
-                    context_list.append(data[i]['context'])
-                input_list = add_prompt_instructions(dimension=dim, output=output_list, 
-                                          src=src_list, context=context_list, task=self.task)
-                score = self.scorer.score(input_list)
+            src_list, output_list, context_list = [], [], []
+            for i in range(n_data):
+                src_list.append(data[i]['source'])
+                output_list.append(data[i]['system_output'])
+                context_list.append(data[i]['context'])
 
-            # Please customize other dimensions here for summarization
-            else:
-                raise NotImplementedError('The input format for this dimension is still undefined. \
-                                           Please customize it first.')
+            score = self.scorer.score(output_list, src_list, context_list, dimension=dim)
             
             for i in range(n_data):
                 eval_scores[i][dim] = score[i]
---
+++ tests/test_evaluator.py
@@ -0,0 +1,31 @@
+# Write a test to evaluate the evaluator
+
+from utils import get_context_data, convert_to_json
+from metric.evaluator import DialogEvaluator
+from utils import generate_samples, evaluate_with_scorer
+from metric.scorer import PromptScorer
+import json
+
+# Dataset specific input collector prepares the data in the format required by the evaluator
+# json_files = [f'{dataset}/{dataset_split}/labels.json', pred_path]
+# datasets = load_jsons(json_files)
+# conv = get_prompt(i, split=dataset_split, label_print=False, max_turns=5, max_n_sent=0, dataset=dataset)[0]
+
+# Candidate responses is a list of strings, turn historys is a list of lists of strings, knowledge_contexts is a list of lists of relevant document strings
+candidate_responses = ["Yes I can. The Cambridge Guest House is a great place to stay. It has free wifi and is in the north of town.",
+                       ""]
+# Example data
+turn_historys = [["Hi, I'm looking for a guest house in the north of town. Is there one with free wifi?"]]
+knowledge_contexts = [[":R: (Cambridge Guest House) Great guest house north of the town.", ":F: (Cambridge Guest House) Do you have free wifi? Yes, we have free wifi."]]
+
+data = convert_to_json(output_list=candidate_responses, src_list=turn_historys, context_list=knowledge_contexts)
+
+llama2local = PromptScorer(api_url="http://gpu-19.apptek.local:8080/generate")
+evaluator = DialogEvaluator(llama2local)
+eval_scores = evaluator.evaluate(data, print_result=True)
+
+# Assert that eval_scores is a list of numeric values corresponding to the length of candidate_responses with one value for each dimension per response
+assert len(eval_scores) == len(candidate_responses)
+print(eval_scores)
+
+# save_files(pred_acc_count, pred_app_count, len(samples), pred_path, winexpls, method)
